2dafe92feec66253ea10c8c03d2eb791c02c953d
leihaowen <leihaowen@miliantech.com>
2025-06-09 02:50:41 +0800

    fix: 更新部署配置以支持静态文件导出

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isExport = process.env.EXPORT_STATIC === 'true';
+
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  output: 'export',
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/zaoqi.icu' : '',
-}
+  images: { unoptimized: true },
+  ...(isExport
+    ? {
+        output: 'export',
+        trailingSlash: true,
+        basePath: '/zaoqi.icu', // 仓库名
+      }
+    : {
+        // SSR 部署时不需要 output 和 basePath
+      }),
+};
 
 module.exports = nextConfig
